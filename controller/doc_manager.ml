0492ecfdaad260c5167ce71971581386a373dca3
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2023-04-07 16:21:27 -0700

    Add --concise option which aggressively reduces the amount of printing

--- controller/doc_manager.ml
+++ controller/doc_manager.ml
@@ -168,7 +168,7 @@ let send_perf_data ~ofn ~(doc : Fleche.Doc.t) =
 module Check : sig
   val schedule : uri:Lang.LUri.File.t -> unit
   val deschedule : uri:Lang.LUri.File.t -> unit
-  val maybe_check : ofn:(Yojson.Safe.t -> unit) -> Int.Set.t option
+  val maybe_check : ofn:(Yojson.Safe.t -> unit) -> concise: bool -> Int.Set.t option
 end = struct
   let pending = ref None
 
@@ -183,16 +183,17 @@ end = struct
     | Stopped _ -> false
 
   (* Notification handling; reply is optional / asynchronous *)
-  let check ~ofn ~uri =
+  let check ~ofn ~concise ~uri =
     LIO.trace "process_queue" "resuming document checking";
     match Handle.find_opt ~uri with
     | Some handle ->
       let target = get_check_target handle.pt_requests in
       let doc = Fleche.Doc.check ~ofn ~target ~doc:handle.doc () in
       let requests = Handle.update_doc_info ~handle ~doc in
-      send_diags ~ofn ~doc;
-      (* Only if completed! *)
-      if completed ~doc then send_perf_data ~ofn ~doc;
+      if not concise then
+        (send_diags ~ofn ~doc;
+         (* Only if completed! *)
+         if completed ~doc then send_perf_data ~ofn ~doc);
       (* Only if completed! *)
       if completed ~doc then pending := None;
       requests
@@ -201,7 +202,7 @@ end = struct
         ("file " ^ Lang.LUri.File.to_string_uri uri ^ " not available");
       Int.Set.empty
 
-  let maybe_check ~ofn = Option.map (fun uri -> check ~ofn ~uri) !pending
+  let maybe_check ~ofn ~concise = Option.map (fun uri -> check ~ofn ~concise ~uri) !pending
   let schedule ~uri = pending := Some uri
 
   let deschedule ~uri =
